b1cf7f86d717b596bc4529bb10d80e20dd2a3a0d
Owner <kurchin.ilya99@gmail.com>
2026-06-15 17:59:10 +0500

    Почти дописал GetResponsesLimit

--- ConverterJSON.cpp
+++ ConverterJSON.cpp
@@ -11,20 +11,52 @@ using json = nlohmann::json;
 using namespace std;
 
 //Метод получения содержимого файлов
-std::vector<std::string>GetTextDocuments() {
+vector<string>GetTextDocuments() {
+
+    vector<string> containFile;
+
     ifstream JSON_file ("config.json");
 
     if (!JSON_file.is_open()) {
         cerr << "File \"config.json\" is not found" << endl;
+        return containFile;
     }
 
+    try {
+        json config_json;
+        JSON_file >> config_json;
+        JSON_file.close();
 
+        if (config_json.contains("files")) {
+            containFile = config_json["files"].get<vector<string>>();
+        } else {
+            cerr << "Field: \"files\" is empty in \"config.json\"" << endl;
+        }
+    } catch (const json::exception &e) {
+        cerr << "JSON parsing error: " << e.what() << endl;
+    }
+    return containFile;
 }
 
 //Метод определения максимального количества ответов на запрос
 int GetResponsesLimit()
 {
+    ifstream JSON_file ("config.json");
 
+    if (!JSON_file.is_open()) {
+        cerr << "File \"config.json\" is not found" << endl;
+        return 1;
+    }
+
+    try {
+        json config_json;
+        JSON_file >> config_json;
+        JSON_file.close();
+
+        if (config_json.contains("config") && config_json["config"].contains("max_respones")) {
+            return config_json["config"]["max_respones"].get<int>();
+        }
+    } // дописать catch и отловить ошибку в случае если не найдено поле max responses
 }
 
 //Метод получения запросов из файла requests.json
